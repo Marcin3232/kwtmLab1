358940dddd98f0bd0f73f78c27ad235d4f332abf
student <student@mini-iris-06.local>
2022-03-16 10:34:13 +0100

    second

---
+++ src/Trygonometria/trygonometria.cpp
@@ -0,0 +1,8 @@
+
+#include "trygonometria.h"
+#include <cmath>
+double degreemath::sin(double degree) {
+    
+    return std::sin(radian);
+}
+//i tak dalej
---
+++ src/Trygonometria/trygonometria.h
@@ -0,0 +1,7 @@
+
+namescape degreemath {
+    double sin(double degree); // i.t.d.
+      double cos(double degree); // i.t.d.
+      double tg(double degree); // i.t.d.
+      double ctg(double degree); // i.t.d.
+}
