51344d41ce871fbd4ee7083131265b2b044e082d
Marcin3232 <58747227+Marcin3232@users.noreply.github.com>
2022-06-08 11:17:57 +0200

    Update trygonometria.h

--- src/Trygonometria/trygonometria.h
+++ src/Trygonometria/trygonometria.h
@@ -2,6 +2,6 @@
 namespace degreemath{
     double sin(double degree); // i.t.d.
       double cos(double degree); // i.t.d.
-      double tg(double degree); // i.t.d.
+      double tg(double degree); // i.t.d. co to ma byc
       double ctg(double degree); // i.t.d.
 }
